025c22feadca77b749c7dd8ac15af6acb82a61e1
Daniel Tull <dt@danieltull.co.uk>
2015-09-16 16:25:21 +0100

    Add swift file that imports CommonCrypto, fails

---
+++ TestCrypto/Test.swift
@@ -0,0 +1,10 @@
+//
+//  Test.swift
+//  TestCrypto
+//
+//  Created by Daniel Tull on 16.09.2015.
+//  Copyright © 2015 Daniel Tull. All rights reserved.
+//
+
+import Foundation
+import CommonCrypto
