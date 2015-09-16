1ff5c66fa68d600c58de1d3b4203413f120b2669
Daniel Tull <dt@danieltull.co.uk>
2015-09-16 16:24:28 +0100

    Initial commit

---
+++ TestCrypto/TestCrypto.h
@@ -0,0 +1,19 @@
+//
+//  TestCrypto.h
+//  TestCrypto
+//
+//  Created by Daniel Tull on 16.09.2015.
+//  Copyright © 2015 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestCrypto.
+FOUNDATION_EXPORT double TestCryptoVersionNumber;
+
+//! Project version string for TestCrypto.
+FOUNDATION_EXPORT const unsigned char TestCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCrypto/PublicHeader.h>
+
+
